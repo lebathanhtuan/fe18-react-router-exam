c6b428887d69776e57570d6dd27af31eb24646e0
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2021-07-13 21:14:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { Space, Button } from 'antd';
+
 import './App.css';
+import 'antd/dist/antd.css';
 
 function App() {
+  const [pageSelected, setPageSelected] = useState('/home');
+
+  function renderPage() {
+    switch (pageSelected) {
+      case '/home': {
+        return (
+          <div>Đây là trang home</div>
+        )
+      }
+      case '/list': {
+        return (
+          <div>Đây là trang list</div>
+        )
+      }
+      case '/detail': {
+        return (
+          <div>Đây là trang detail</div>
+        )
+      }
+      case '/profile': {
+        return (
+          <div>Đây là trang profile</div>
+        )
+      }
+      default: {
+        return (
+          <div>Đây là trang home</div>
+        )
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Space size={32}>
+        <Button onClick={() => setPageSelected('/home')}>Home</Button>
+        <Button onClick={() => setPageSelected('/list')}>List</Button>
+        <Button onClick={() => setPageSelected('/detail')}>Detail</Button>
+        <Button onClick={() => setPageSelected('/profile')}>Profile</Button>
+      </Space>
+      <div>
+        {renderPage()}
+      </div>
     </div>
   );
 }
